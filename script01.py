f1ca3808e892f65673da4ba81d734bfb2c4d7075
Brandon Gottfried <bgottfried@medicom.us>
2018-05-15 17:51:45 -0400

    script to mass generate latlong objects from file

---
+++ script01.py
@@ -0,0 +1,8 @@
+with open('./cities', 'r') as infile, open('./latlongs', 'a') as outfile:
+    for line in infile:
+        arr = line.split()
+        arr = arr[(len(arr)-2):]
+        latlong = "lat: {}, lng: {}".format(arr[0], arr[1])
+        latlong = "{" + latlong + "}, \n"
+        outfile.write(latlong)
+infile.close()
