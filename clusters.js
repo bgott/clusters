6146a5619a3f0bc16904d5f317f0d3c14cebff12
Brandon Gottfried <bgottfried@medicom.us>
2018-05-15 15:13:39 -0400

    add map clustering; map reset

--- clusters.js
+++ clusters.js
@@ -10,6 +10,15 @@ var map_styles = [
         ]
     },
     {
+        "featureType": "all",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
         "featureType": "administrative.country",
         "elementType": "all",
         "stylers": [
@@ -64,8 +73,26 @@ var map_styles = [
         ]
     },
     {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
         "featureType": "administrative.land_parcel",
         "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
         "stylers": [
             {
                 "visibility": "off"
@@ -82,8 +109,35 @@ var map_styles = [
         ]
     },
     {
+        "featureType": "landscape.natural.landcover",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
         "featureType": "poi",
         "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
         "stylers": [
             {
                 "visibility": "off"
@@ -122,14 +176,50 @@ var map_styles = [
                 "saturation": -60
             }
         ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
     }
+];
+var locations = [
+    {lat: 42.40, lng: -95},
+    {lat: 42.0, lng: -95.7129},
+    {lat: 41.0, lng: -96},
 ];
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 38.5902, lng: -95.7129},
-    zoom: 5,
-    styles: map_styles
-  });
-  console.log("init map exiting...");
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 38.5902, lng: -95.7129},
+        zoom: 5,
+        styles: map_styles,
+        disableDefaultUI: true
+    });
+
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+
+    var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
+
+function resetMap(){
+    initMap();
+}
+
+document.getElementById('title').onclick = function() {
+    map.setCenter({lat: 38.5902, lng: -95.7129});
+    map.setZoom(5);
+};
